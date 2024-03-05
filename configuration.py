9b9831bc578304d314031061822cf701920f140b
sonnike <kijo5513@gmail.com>
2024-03-05 22:15:49 +0600

    My first commit

---
+++ configuration.py
@@ -0,0 +1,12 @@
+# URL_SERVICE хранит базовый URL веб-сервиса, который используется для доступа к API или другим ресурсам.
+# Значение должно быть скопировано из настроек или документации сервиса, к которому предоставляется доступ.
+# Пример значения: "https://api.example.com"
+URL_SERVICE = "https://6f202dd6-259e-4021-8cfd-0acd1d1f822a.serverhub.praktikum-services.ru"
+
+# DOC_PATH содержит путь к документации веб-сервиса.
+# Этот путь используется для формирования полного URL пути к документации, добавляя его к базовому URL сервиса.
+# В результате получится что-то вроде "https://api.example.com/docs/"
+DOC_PATH = "/docs/"
+
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
